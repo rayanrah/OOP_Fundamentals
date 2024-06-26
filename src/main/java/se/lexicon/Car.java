b73319627a416f901484d827c337ea9724cdbcda
ryan.Rah <re.rahdari@gmail.com>
2024-06-26 13:18:13 +0200

    Encapsulation principle.

--- src/main/java/se/lexicon/Car.java
+++ src/main/java/se/lexicon/Car.java
@@ -9,10 +9,10 @@ public class Car {
 
     // Fields (Variables): we need to specify the fields for Car template.
     // these variables can store the data of the object.
-    String registrationNumber;
-    String brand;
-    String model;
-    int year;
+    private String registrationNumber;
+    private String brand;
+    private String model;
+    private int year;
 
     /* Create object in APP class:
     To create an object (instances of Car), we use the "new" keyword
@@ -31,7 +31,7 @@ public class Car {
     - There will be 'no default constructor' with empty fields after we create this one here.
     - For now, you have to add the value to fields when you create a new object in App class --> Car newCar = new Car();
     */
-    public Car (String regNumber, String brand, String model, int year) {
+    public Car(String regNumber, String brand, String model, int year) {
         registrationNumber = regNumber;
         this.brand = brand;
         this.model = model;
@@ -53,22 +53,38 @@ public class Car {
         }
     */
 
-    public Car (String brand, String model){
-        this.registrationNumber = UUID.randomUUID().toString().substring(0,8); // Generate random number.
+    public Car(String brand, String model) {
+        this.registrationNumber = UUID.randomUUID().toString().substring(0, 8); // Generate random number.
         this.brand = brand;
         this.model = model;
         this.year = LocalDate.now().getYear();
     }
 
-    // Methods (Behaviour) -
+    // Getter Methods (Behaviour):
+    // It provides read-only information to print.
     // The purpose of Method is to collect the information of instance data and return it to the text sentence.
     // when we call 'getCarInfo', it should provide the summary information about the car.
-    public String getCarInfo () {
+    public String getCarInfo() {
         String result = "Car info -> Registration Number: " + registrationNumber + " , brand: " + brand + ", Model: " + model + ", Year: " + year;
         return result;
 
     }
 
+    /* info: Encapsulation principle:
+     - You need to make the field private to protect them from changing.
+     - create a setter for some of the fields.
+     - It is a process of hiding or protecting the data.
+     - for this project, we set 'brand' as Encapsulation principle.
+    */
 
+    // create a setter method:
+    // We can now be able to change the Brand name inside the App --> Car1.setBrand("NewVolvo");
+    // We can use the 'setBrand ()' method directly at our constructor by replacing it with 'this.brand = brand;'
 
+    public void setBrand(String brand) {
+        if (brand == null || brand.trim().isEmpty()) {
+            throw new IllegalArgumentException("brand can not be null or empty.");
+        }
+        this.brand = brand;
+    }
 }
